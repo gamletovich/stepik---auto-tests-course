b994b74bfd13b082ada2442f4fea4c15bb70b20b
Armen Boiadzhian <z@fastlive.eu>
2020-01-03 13:42:49 +0100

    adding time sleep and assert

--- 2_review_task(conftest)/test_items.py
+++ 2_review_task(conftest)/test_items.py
@@ -1,6 +1,9 @@
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#add_to_basket_form")
+    time.sleep(5)
+    add_button_text = browser.find_element_by_css_selector("#add_to_basket_form").text
+    assert add_button_text,  "There is no button 'Add to basket'"
