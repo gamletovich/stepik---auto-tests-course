2edda77cf6c8ffc583ac6ccd96897c7b63d9e39f
Armen Boiadzhian <z@fastlive.eu>
2020-01-03 14:06:39 +0100

    renaming back the method

--- 2_review_task(conftest)/test_items.py
+++ 2_review_task(conftest)/test_items.py
@@ -2,7 +2,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_add_button_checker(browser):
     browser.get(link)
     time.sleep(5)
     add_button_text = browser.find_element_by_css_selector("#add_to_basket_form").text
