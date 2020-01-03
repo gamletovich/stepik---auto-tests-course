835dc1dd6f796deada39d93107c64ce94b3342e7
Armen Boiadzhian <z@fastlive.eu>
2020-01-03 14:08:33 +0100

    changing time sleep

--- 2_review_task(conftest)/test_items.py
+++ 2_review_task(conftest)/test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_button_checker(browser):
     browser.get(link)
-    time.sleep(5)
+    time.sleep(3)
     add_button_text = browser.find_element_by_css_selector("#add_to_basket_form").text
     assert add_button_text,  "There is no button 'Add to basket'"
