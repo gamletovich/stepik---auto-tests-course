09186a22ab70d46eacce3acbc719168720849bd8
Armen Boiadzhian <z@fastlive.eu>
2020-01-03 11:48:16 +0100

    Distributing tasks into folders and adding a second conftest task.

---
+++ 2_review_task(conftest)/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="ru",
+                     help="-")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
+    browser = webdriver.Chrome('C:\\Users\\armen.boiadzhian\\Downloads\\Armen\\Projects\\chromedriver.exe', options=options)
+    yield browser
+
+    print("\nquit browser..")
+    browser.quit()
