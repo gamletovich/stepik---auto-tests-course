1bc1e12293fcf4c6cf724695ce4437da85ce24ca
Armen Boiadzhian <z@fastlive.eu>
2020-01-03 12:00:19 +0100

    https://stepik.org/lesson/187065/step/11?unit=161976

--- 2_review_task(conftest)/test_items.py
+++ 2_review_task(conftest)/test_items.py
@@ -3,4 +3,4 @@ link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element_by_css_selector ("#add_to_basket_form")
+    browser.find_element_by_css_selector("#add_to_basket_form")
