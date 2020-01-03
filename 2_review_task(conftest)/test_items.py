7590b71fd23f2a11c9aafbca07c7c3443e73eac2
Armen Boiadzhian <z@fastlive.eu>
2020-01-03 12:12:24 +0100

    Test Case - Checking "Add to basket" button

--- 2_review_task(conftest)/test_items.py
+++ 2_review_task(conftest)/test_items.py
@@ -1,6 +1,6 @@
 link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket_checker(browser):
     browser.get(link)
     browser.find_element_by_css_selector("#add_to_basket_form")
