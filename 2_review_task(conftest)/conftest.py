917bd04228a5ecd9bae9d3c48b136d46cf1da920
Armen Boiadzhian <z@fastlive.eu>
2020-01-03 12:17:27 +0100

    conftest, default language is Russian, work with Chrome

--- 2_review_task(conftest)/conftest.py
+++ 2_review_task(conftest)/conftest.py
@@ -4,8 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="ru",
-                     help="-")
+    parser.addoption('--language', action='store', default="ru", help="-")
 
 
 @pytest.fixture(scope="function")
