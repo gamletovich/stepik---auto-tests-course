3af285f7ccd9136d0116256cbd2009920191219d
armen.moiadzhian <z@fastlive.eu>
2020-01-06 09:33:44 +0100

    -

--- 2_review_task(conftest)/conftest.py
+++ 2_review_task(conftest)/conftest.py
@@ -33,4 +33,4 @@ def browser(request):
                                 "language should be in short form (en, uk, ru)")
     yield browser
     print("\nquit browser..")
-    browser.quit( )
+    browser.quit()
